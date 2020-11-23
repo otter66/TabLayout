a4446b9983f59bad5b3772d033d314e5aa1eacfe
otter66 <otter66@kakao.com>
2020-11-23 20:53:46 +0900

    [Add] TabLayout Study Log

---
+++ app/src/main/java/com/example/recyclerviewexample/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.example.recyclerviewexample
+
+import StupidFragment
+import TestFragment
+import android.annotation.SuppressLint
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : BaseActivity() {
+
+    override var viewId: Int = R.layout.activity_main
+    override var toolbarId: Int? = R.id.toolbar
+    private lateinit var toast: Toast
+    private var backKeyPressedTime: Long = 200
+    private var mViewPager: ViewPager? = null
+
+    @SuppressLint("ShowToast")
+    override fun onCreate() {
+        showActionBar()
+
+
+        mViewPager = findViewById(R.id.viewPager)
+        mViewPager!!.adapter = PagerAdapter(supportFragmentManager)
+        mViewPager!!.currentItem = 0
+
+        val tabLayout = findViewById<View>(R.id.tabs) as TabLayout
+        tabLayout.setupWithViewPager(mViewPager)
+
+
+        tabLayout.getTabAt(0)!!.text = "1"
+        tabLayout.getTabAt(1)!!.text = "2"
+        tabLayout.getTabAt(2)!!.text = "3"
+        tabLayout.getTabAt(3)!!.text = "4"
+
+        mViewPager!!.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) = tab.select()
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+            }
+        })
+
+    }
+
+    override fun onBackPressed() {
+
+        if (System.currentTimeMillis() > backKeyPressedTime + 500) {
+            backKeyPressedTime = System.currentTimeMillis()
+            toast.show()
+            return
+        }
+
+        if (System.currentTimeMillis() <= backKeyPressedTime + 500) {
+            toast.cancel()
+            this.finish()
+        }
+    }
+
+    inner class PagerAdapter(supportFragmentManager: FragmentManager) : FragmentStatePagerAdapter(supportFragmentManager) {
+
+        override fun getItem(position: Int): Fragment {
+
+            return when (position) {
+                0 ->
+                    StupidFragment()
+                1 ->
+                    TestFragment()
+                2 ->
+                    StupidFragment()
+                3 ->
+                    TestFragment()
+                else ->
+                    TestFragment()
+            }
+        }
+
+        override fun getCount(): Int = 4
+    }
+}
---
+++ app/src/main/java/com/example/recyclerviewexample/TestFragment.kt
@@ -0,0 +1,13 @@
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.recyclerviewexample.R
+
+class TestFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_test, container, false)
+    }
+}
